2cc3d983e9e123a873e5b2d15c105c47340dd460
Saymon <simon.daniau@gmail.com>
2016-02-10 00:27:01 +0100

    Première passe
    Tourelle fonctionelle

---
+++ Assets/Projectile_Cannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_Cannon : MonoBehaviour
+{
+
+	public float speed = 10f;
+	public float range = 10f;
+
+	float distance;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		distance += Time.deltaTime * speed;
+		if (distance >= range) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Turret_Basic.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret_Basic : MonoBehaviour
+{
+
+	// Use this for initialization
+
+	public GameObject projectile;
+	public float reloadTime = 1f;
+	public float turnSpeed = 5f;
+	public float firePauseTime = .25f;
+	//public GameObject muzzleFlash;
+	public float errorAmount = .001f;
+	public Transform target;
+	public Transform[] muzzle;
+	public Transform ball;
+
+	float nextFireTime;
+	float nextMoveTime;
+	Quaternion desiredRot;
+	float aimError;
+
+	
+	// Update is called once per frame
+
+	void Start ()
+	{
+		nextFireTime = Time.time + reloadTime;
+	}
+
+	void Update ()
+	{
+		if (target) {
+			if (Time.time >= nextMoveTime) {
+				CalculateAimPos (target.position);
+				ball.rotation = Quaternion.Lerp (ball.rotation, desiredRot, Time.deltaTime * turnSpeed);
+			}
+			if (Time.time >= nextFireTime) {
+				FireProjectile ();
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			nextFireTime = Time.time + (reloadTime * 0.5f);
+			target = other.gameObject.transform;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.transform == target) {
+			target = null;
+		}
+	}
+
+	void CalculateAimPos (Vector3 pos)
+	{
+		Vector3 aimpoint = new Vector3 (pos.x + aimError, pos.y + aimError, pos.z + aimError);
+		desiredRot = Quaternion.LookRotation (aimpoint);
+	}
+
+	void CalculateAimError ()
+	{
+		aimError = Random.Range (-errorAmount, errorAmount);
+	}
+
+	void FireProjectile ()
+	{
+		//Audio.Play();
+		nextFireTime = Time.time + reloadTime;
+		nextMoveTime = Time.time + firePauseTime;
+		CalculateAimError ();
+		foreach (Transform m in muzzle) {
+			Instantiate (projectile, m.position, m.rotation);
+			//Instantiate (muzzleFlash, m.position, m.rotation);
+		}
+	}
+
+
+
+
+}
